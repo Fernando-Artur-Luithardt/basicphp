3f3a19abaf9130f59180487fe78c14bc013c0055
Fernando-Artur-Luithardt <fluithardt@gmail.com>
2021-12-03 19:27:18 -0300

    Update banco.php

---
+++ banco.php
@@ -0,0 +1,11 @@
+<?php
+
+    //conectar banco de dados
+    $host = "localhost"; //onde está
+    $user = "fernando_user"; // usuario
+    $pass = "99696712"; // qual a senha
+    $base = "fernando_base"; // qual a base de dados
+    
+    $conn = mysqli_connect($host, $user, $pass, $base);
+    
+?>
